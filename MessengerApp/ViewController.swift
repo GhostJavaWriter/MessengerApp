e856d17df8bfbdcca4f4ac8b0033ad3f58ae9d0e
Bair Nadtsalov <nadtsalov@gmail.com>
2021-02-13 11:30:09 +0800

    feat: add pinting log info for viewController

--- MessengerApp/ViewController.swift
+++ MessengerApp/ViewController.swift
@@ -14,6 +14,39 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("ViewController : " + #function)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("ViewController : " + #function)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        
+        print("ViewController : " + #function)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+        print("ViewController : " + #function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("ViewController : " + #function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("ViewController : " + #function)
+    }
 
 }
 
