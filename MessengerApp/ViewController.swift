8b94b420f902beed3159daa95ce93812a1ab8737
Bair Nadtsalov <nadtsalov@gmail.com>
2021-02-13 16:34:12 +0800

    feat: add NSLog switch

--- MessengerApp/ViewController.swift
+++ MessengerApp/ViewController.swift
@@ -11,41 +11,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NSLog("ViewController : \(#function)")
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        print("ViewController : " + #function)
+        NSLog("ViewController : " + #function)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        print("ViewController : " + #function)
+       NSLog("ViewController : " + #function)
     }
     
     override func viewWillLayoutSubviews() {
         
-        print("ViewController : " + #function)
+        NSLog("ViewController : " + #function)
     }
     
     override func viewDidLayoutSubviews() {
         
-        print("ViewController : " + #function)
+        NSLog("ViewController : " + #function)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        print("ViewController : " + #function)
+        NSLog("ViewController : " + #function)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         
-        print("ViewController : " + #function)
+        NSLog("ViewController : " + #function)
     }
 
 }
