50491dd76b5de546f7fcbdfe76442180fc609551
Bair Nadtsalov <nadtsalov@gmail.com>
2021-03-27 20:57:54 +0800

    feat: add CoreDataStack file and xcdatamodel

---
+++ MessengerApp/CoreDataManager/CoreDataStack.swift
@@ -0,0 +1,150 @@
+//
+//  CoreDataStack.swift
+//  MessengerApp
+//
+//  Created by Bair Nadtsalov on 27.03.2021.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    var didUpdateDataBase: ((CoreDataStack) -> Void)?
+    
+    private var storeURL: URL = {
+        guard let documentsURL = FileManager.default.urls(for: .documentDirectory,
+                                                          in: .userDomainMask).last else {
+            fatalError("Document path not found")
+        }
+        return documentsURL
+    }()
+    
+    private let dataModelName = "ChatStorage"
+    private let dataModelExtension = "momd"
+    
+    // MARK: - init Stack
+    
+    private(set) lazy var managedObjectModel: NSManagedObjectModel = {
+        guard let modelURL = Bundle.main.url(forResource: dataModelName,
+                                             withExtension: dataModelExtension) else {
+            fatalError("model not found")
+        }
+        
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("managedObjectModel could not be created")
+        }
+        
+        return managedObjectModel
+    }()
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        
+        do {
+            try coordinator.addPersistentStore(ofType: NSSQLiteStoreType,
+                                               configurationName: nil,
+                                               at: self.storeURL,
+                                               options: nil)
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+        return coordinator
+    }()
+    
+    // MARK: - Contexts
+    
+    private lazy var writtenContext: NSManagedObjectContext = {
+        let context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        context.persistentStoreCoordinator = persistentStoreCoordinator
+        context.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
+        return context
+    }()
+    
+    private(set) lazy var mainContext: NSManagedObjectContext = {
+        let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        context.parent = writtenContext
+        context.automaticallyMergesChangesFromParent = true
+        context.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
+        return context
+    }()
+    
+    private func saveContext() -> NSManagedObjectContext {
+        let context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        context.parent = mainContext
+        context.automaticallyMergesChangesFromParent = true
+        context.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
+        return context
+    }
+    
+    // MARK: - Save Context
+    
+    func performSave(_ block: (NSManagedObjectContext) -> Void) {
+        let context = saveContext()
+        context.performAndWait {
+            block(context)
+            if context.hasChanges {
+                performSave(in: context)
+            }
+        }
+    }
+    
+    private func performSave(in context: NSManagedObjectContext) {
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch {
+                assertionFailure(error.localizedDescription)
+            }
+        }
+        if let parent = context.parent { performSave(in: parent) }
+    }
+    
+    // MARK: - CoreData Observers
+    
+    func enableObservers() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self,
+                                       selector: #selector(managedObjectContextObjectsDidChange(notification:)),
+                                       name: NSNotification.Name.NSManagedObjectContextObjectsDidChange,
+                                       object: mainContext)
+    }
+    
+    @objc
+    private func managedObjectContextObjectsDidChange(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else { return }
+        
+        didUpdateDataBase?(self)
+        
+        if let inserts = userInfo[NSInsertedObjectsKey] as? Set<NSManagedObject>,
+           inserts.count > 0 {
+            print("Добавлено объектов: ", inserts.count)
+        }
+        
+        if let updates = userInfo[NSUpdatedObjectsKey] as? Set<NSManagedObject>,
+           updates.count > 0 {
+            print("Обновлено объектов: ", updates.count)
+        }
+        
+        if let deletes = userInfo[NSDeletedObjectsKey] as? Set<NSManagedObject>,
+           deletes.count > 0 {
+            print("Удалено объектов: ", deletes.count)
+        }
+    }
+
+    // MARK: - Core Data Logs
+    
+    func printDatabaseStatistics() {
+        mainContext.perform {
+            do {
+                let count = try self.mainContext.count(for: User.fetchRequest())
+                print("\(count) пользователей")
+                let array = try self.mainContext.fetch(User.fetchRequest()) as? [User] ?? []
+                array.forEach {
+                    print($0.about)
+                }
+            } catch {
+                fatalError(error.localizedDescription)
+            }
+        }
+    }
+}
